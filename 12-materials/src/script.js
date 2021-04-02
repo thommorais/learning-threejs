950c0cfdb50135b443a0197341505d6d7e8f4e7b
thommorais <thommorais@gmail.com>
2021-04-02 01:13:20 -0300

    add env maps

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -42,6 +42,9 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
 scene.add(ambientLight)
 
 const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 1
+pointLight.position.y = 3
+pointLight.position.z = 4
 scene.add(pointLight)
 
 
@@ -63,37 +66,72 @@ scene.add(pointLight)
 // gradientTexture.minFilter = THREE.NearestFilter
 // gradientTexture.magFilter = THREE.NearestFilter
 // gradientTexture.generateMipmaps = false
+
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+
+const ambient = 0
+
+const enviromentMapTexture = cubeTextureLoader.load([
+    `/textures/environmentMaps/${ambient}/px.jpg`,
+    `/textures/environmentMaps/${ambient}/nx.jpg`,
+    `/textures/environmentMaps/${ambient}/py.jpg`,
+    `/textures/environmentMaps/${ambient}/ny.jpg`,
+    `/textures/environmentMaps/${ambient}/pz.jpg`,
+    `/textures/environmentMaps/${ambient}/nz.jpg`,
+])
+
 /**
  * Objects
  */
 
 const material = new THREE.MeshStandardMaterial()
-material.metalness = 0.45
+
+// material.map = doorColorTexture
+// material.aoMap = doorAmbientOcclusionTexture
+// material.displacementMap = doorHeightTexture
+// material.metalnessMap = doorMetalnessTexture
+// material.roughnessMap = doorRoughnessTexture
+// material.normalMap = doorNormalTexture
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+
+// material.displacementScale = 0.1
+// material.aoMapIntensity = 2
+material.metalness = 1
+material.roughness = 0
+material.envMap = enviromentMapTexture
+// material.normalScale.set(0.2, 0.2)
+
 
 gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+gui.add(material, 'roughness').min(0).max(1).step(0.0001)
 
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    material
-)
-sphere.position.x = -1.5
+// gui.add(material, 'aoMapIntensity').min(0).max(10).step(0.1)
+// gui.add(material, 'displacementScale').min(0).max(10).step(0.1)
+// gui.set(material, 'normalScale').min(0).max(10).step(0.1)
 
 
 const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(1, 1),
+    new THREE.PlaneGeometry(1, 1, 64, 64),
     material
 )
 
 plane.material.side = THREE.DoubleSide
 
-const torus = new THREE.Mesh(
-    new THREE.TorusGeometry(0.3, 0.2, 16, 32),
+
+plane.geometry.setAttribute('uv2', new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2))
+
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 120, 120),
     material
 )
+sphere.geometry.setAttribute('uv2', new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2))
+sphere.position.x = - 1.5
 
-torus.position.x = 1.5
 
-scene.add(sphere, plane, torus)
+scene.add(plane, sphere)
 
 window.addEventListener('resize', () =>
 {
@@ -116,7 +154,7 @@ window.addEventListener('resize', () =>
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 1
-camera.position.y = 1
+camera.position.y = 2
 camera.position.z = 3
 scene.add(camera)
 
@@ -144,15 +182,17 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
 
-    sphere.rotation.y = elapsedTime * 0.1
-    sphere.rotation.x = elapsedTime * 0.5
+    // sphere.rotation.y = elapsedTime * 0.1
+    // sphere.rotation.x = elapsedTime * 0.5
 
-    plane.rotation.y = elapsedTime * 0.1
-    plane.rotation.x = elapsedTime * -0.3
+    // plane.rotation.y = elapsedTime * 0.1
+    // plane.rotation.x = elapsedTime * -0.3
 
-    torus.rotation.y = elapsedTime * 0.1
-    torus.rotation.x = elapsedTime * 0.5
+    // torus.rotation.y = elapsedTime * 0.1
+    // torus.rotation.x = elapsedTime * 0.5
 
+    // camera.position.x = Math.cos(elapsedTime * 0.5)
+    // camera.position.y = Math.sin(elapsedTime * 0.5)
 
     // Update controls
     controls.update()
